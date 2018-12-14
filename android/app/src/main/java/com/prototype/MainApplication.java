adf3ae19e1d9b7bcbcd28bd3cac2c5147b78f262
Mark Sierikov <serikov.lg@gmail.com>
2018-12-14 17:57:16 +0200

    added Splash Screen

--- android/app/src/main/java/com/prototype/MainApplication.java
+++ android/app/src/main/java/com/prototype/MainApplication.java
@@ -1,8 +1,12 @@
 package com.prototype;
 
 import android.app.Application;
+import android.util.Log;
+
+import com.facebook.react.BuildConfig;
 
 import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
